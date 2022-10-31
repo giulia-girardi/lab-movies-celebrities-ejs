e37f2af522ba2a0d2ff89af4cdda0a16a3b60102
Giulia Girardi <gg.giuliagirardi@gmail.com>
2022-10-31 17:16:23 +0100

    done iteration3

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,4 +1,4 @@
-const {Schema, model} = require(mongoose)
+const {Schema, model} = require('mongoose')
 
 const CelebritySchema = new Schema({
     name: String, 
--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,8 +1,25 @@
 // starter code 
 const router = require('express').Router()
-
+const CelebrityModel = require('../models/Celebrity.model.js')
 // all your routes here
 
+router.get('/create', (req, res)=> {
+    res.render('celebrities/new-celebrity')
+})
 
+router.post('/create', async (req, res)=> {
+    try {
+        const celebrity1 = await CelebrityModel.create({
+            name: req.body.name,
+            occupation: req.body.occupation,
+            catchPhrase: req.body.catchPhrase,
+        })
+        res.redirect('celebrities', {celebrity1})
+    } 
+    catch(err) {
+        res.render('celebrities/new-celebrity')
+    }
+
+})
 
 module.exports = router
